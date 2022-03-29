cfed60cf154c0b98b41249e110eff0be8f6256b7
Joaquin <34746495+cojua8@users.noreply.github.com>
2022-03-28 21:37:37 -0300

    add black config and format all

--- src/app/services/json_database/todos_json_database_service.py
+++ src/app/services/json_database/todos_json_database_service.py
@@ -1,5 +1,7 @@
 from app.models.todo import Todo
-from app.services.json_database.json_database_service import JsonDatabaseService
+from app.services.json_database.json_database_service import (
+    JsonDatabaseService,
+)
 
 
 class TodosJsonDatabaseService(JsonDatabaseService[Todo]):
