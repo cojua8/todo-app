fd497e03b751c6c5524b1a125c7f7c80bca0147b
Joaquín Neira <joaquin.neiraj@gmail.com>
2022-08-28 00:33:07 -0400

    upgrade svelte kit to latest version

---
+++ src/frontend/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+    import NavBar from "../components/NavBar.svelte";
+
+    import "../app.css";
+
+    let year = new Date().getFullYear();
+</script>
+
+<NavBar />
+<div>
+    <div class="mt-20">
+        <slot />
+    </div>
+
+    <p class="text-center text-gray-500 text-xs">
+        &copy;{year} Joaquin. All rights reserved.
+    </p>
+</div>
