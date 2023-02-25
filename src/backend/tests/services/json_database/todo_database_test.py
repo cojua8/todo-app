04d75b33b65b75a230584b47db8e6f66c2f94fdb
Joaquín Neira <joaquin.neiraj@gmail.com>
2023-02-25 14:02:08 -0300

    fix tests

--- src/backend/tests/services/json_database/todo_database_test.py
+++ src/backend/tests/services/json_database/todo_database_test.py
@@ -1,8 +1,6 @@
-from typing import cast
 from uuid import uuid4
 
 import pytest
-from app.models.todo import Todo
 from app.services.json_database_service.todos_json_database_service import (
     TodosJsonDatabaseService,
 )
@@ -14,84 +12,85 @@ from tests.factories.todo_factory import TodoFactory
 
 
 @pytest.fixture
-def setup(request, todo_factory: TodoFactory):
-    expected_values = request.param[0]
+def setup_todos(request, todo_factory: TodoFactory):
+    has_params = hasattr(request, "param")
+    expected_values = request.param[0] if has_params else {}
     expected_todo = todo_factory.create(**expected_values)
 
-    existing_todo = todo_factory.create_batch(request.param[1])
+    existing_todo = todo_factory.create_batch(
+        request.param[1] if has_params else 1
+    )
 
     db_todos = [expected_todo, *existing_todo]
 
-    yield json_utils.dumps(db_todos), expected_todo
+    yield db_todos, expected_todo
+
+
+@pytest.fixture
+def setup_service(mocker):
+    io_service = mocker.MagicMock(spec=IOServiceProtocol)
+    service = TodosJsonDatabaseService(io_service)
+
+    yield service, io_service
 
 
 @pytest.mark.parametrize(
-    "setup",
+    "setup_todos",
     [({}, 4), ({}, 0)],
     indirect=True,
     ids=["return_todo", "return_empty"],
 )
-def test_get_all_ok(mocker, setup):
+def test_get_all_ok(setup_todos, setup_service):
     # arrange
-    todos, _ = setup
-    expected_todos = [Todo(**v) for v in json_utils.loads(todos)]
-    io_service = mocker.MagicMock(spec=IOServiceProtocol)
-    io_service.read.return_value = todos
-
-    service = TodosJsonDatabaseService(io_service)
-    # act
-    actual_todos = service.get_all()
-
-    # assert
-    assert actual_todos == expected_todos
-
-
-@pytest.mark.parametrize(
-    "setup",
-    [({"id": uuid4()}, 4), ({}, 0)],
-    indirect=True,
-    ids=["return_todo", "return_none"],
-)
-def test_get_returns_user(mocker, setup):
-    # arrange
-    todos, expected_todo = setup
-    io_service = mocker.MagicMock(spec=IOServiceProtocol)
-    io_service.read.return_value = todos
-
-    service = TodosJsonDatabaseService(io_service)
-    # act
-    actual_todo = cast(Todo, service.get(expected_todo.id))
-
-    # assert
-    if actual_todo:
-        assert actual_todo.id == expected_todo.id
-
-
-def test_get_returns_none(mocker, todo_factory: TodoFactory):
-    # arrange
-    id_ = uuid4()
-    todos = todo_factory.create_batch(4)
-
-    io_service = mocker.MagicMock(spec=IOServiceProtocol)
+    todos, _ = setup_todos
+    # expected_todos = [Todo(**v) for v in json_utils.loads(todos)]
+    todo_service, io_service = setup_service
     io_service.read.return_value = json_utils.dumps(todos)
 
-    service = TodosJsonDatabaseService(io_service)
     # act
-    actual_todo = cast(Todo, service.get(id_))
+    actual_todos = todo_service.get_all()
+
+    # assert
+    assert actual_todos == todos
+
+
+@pytest.mark.parametrize("setup_todos", [({"id": uuid4()}, 4)], indirect=True)
+def test_get_returns_todo(setup_todos, setup_service):
+    # arrange
+    todos, expected_todo = setup_todos
+    todo_service, io_service = setup_service
+    io_service.read.return_value = json_utils.dumps(todos)
+
+    # act
+    actual_todo = todo_service.get(expected_todo.id)
+
+    # assert
+    assert actual_todo.id == expected_todo.id
+
+
+@pytest.mark.parametrize("setup_todos", [({}, 1)], indirect=True)
+def test_get_returns_none(setup_todos, setup_service):
+    # arrange
+    todos, _ = setup_todos
+    todo_service, io_service = setup_service
+    io_service.read.return_value = json_utils.dumps(todos)
+    searched_id = uuid4()
+
+    # act
+    actual_todo = todo_service.get(searched_id)
 
     # assert
     assert actual_todo is None
 
 
-def test_create_ok(mocker, todo_factory: TodoFactory):
+def test_create_ok(setup_service, todo_factory):
     # arrange
     todo = todo_factory.create()
-    io_service = mocker.MagicMock(spec=IOServiceProtocol)
+    todo_service, io_service = setup_service
     io_service.read.return_value = "[]"
 
-    service = TodosJsonDatabaseService(io_service)
     # act
-    service.create(todo)
+    todo_service.create(todo)
 
     # assert
     io_service.write.assert_called_once_with(
@@ -99,38 +98,53 @@ def test_create_ok(mocker, todo_factory: TodoFactory):
     )
 
 
-@pytest.mark.parametrize("setup", [({}, 4)], indirect=True)
-def test_delete_ok(mocker, setup):
+@pytest.mark.parametrize("setup_todos", [({}, 4)], indirect=True)
+def test_delete_ok(setup_service, setup_todos):
     # arrange
-    todos, expected_todo = setup
-    io_service = mocker.MagicMock(spec=IOServiceProtocol)
-    io_service.read.return_value = todos
+    todos, expected_todo = setup_todos
+    todo_service, io_service = setup_service
+    io_service.read.return_value = json_utils.dumps(todos)
 
-    service = TodosJsonDatabaseService(io_service)
     # act
-    service.delete(expected_todo.id)
+    todo_service.delete(expected_todo.id)
 
     # assert
     io_service.write.assert_called_once_with(
-        json_utils.dumps(json_utils.loads(todos)[1:], indent=4)
+        json_utils.dumps(todos[1:], indent=4)
     )
 
 
-@pytest.mark.parametrize("setup", [({}, 4)], indirect=True)
-def test_put_ok(mocker, setup):
+@pytest.mark.parametrize("setup_todos", [({}, 4)], indirect=True)
+def test_put_ok(setup_todos, setup_service, todo_factory):
     # arrange
-    todos, expected_todo = setup
-    updated_todo = TodoFactory.create(id=expected_todo.id)
-    expected_todos = [updated_todo, *json_utils.loads(todos)[1:]]
+    todos, expected_todo = setup_todos
+    updated_todo = todo_factory.create(id=expected_todo.id)
+    expected_todos = [updated_todo, *todos[1:]]
 
-    io_service = mocker.MagicMock(spec=IOServiceProtocol)
-    io_service.read.return_value = todos
+    todo_service, io_service = setup_service
+    io_service.read.return_value = json_utils.dumps(todos)
 
-    service = TodosJsonDatabaseService(io_service)
     # act
-    service.put(expected_todo.id, updated_todo)
+    todo_service.put(expected_todo.id, updated_todo)
 
     # assert
     io_service.write.assert_called_once_with(
         json_utils.dumps(expected_todos, indent=4)
     )
+
+
+def test_get_all_by_user_id_ok(setup_service, setup_todos, todo_factory):
+    # arrange
+    todos, expected_todo = setup_todos
+    owner_id = expected_todo.owner_id
+    second_todo = todo_factory.create(owner_id=owner_id)
+    expected_todos = [second_todo, *todos]
+
+    todos_service, io_service = setup_service
+    io_service.read.return_value = json_utils.dumps(expected_todos)
+
+    # act
+    actual_todos = todos_service.get_all_by_user_id(owner_id)
+
+    # assert
+    assert actual_todos == [second_todo, expected_todo]
