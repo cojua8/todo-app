1b380b3155fffa10e9315c3c3d141fe207970ec3
Joaquín Neira <joaquin.neiraj@gmail.com>
2023-02-25 14:01:50 -0300

    date psarsing

--- src/backend/app/utils/json_utils.py
+++ src/backend/app/utils/json_utils.py
@@ -1,13 +1,13 @@
+import datetime as dt
 import json
 from dataclasses import is_dataclass
-from datetime import date
 from uuid import UUID
 
 
 class _EnhancedJSONEncoder(json.JSONEncoder):
     def default(self, o):
-        if isinstance(o, date):
-            return date.isoformat(o)
+        if isinstance(o, dt.date):
+            return dt.date.isoformat(o)
         elif isinstance(o, UUID):
             return o.hex
         elif is_dataclass(o):
@@ -15,16 +15,48 @@ class _EnhancedJSONEncoder(json.JSONEncoder):
         return super().default(o)
 
 
-def _id_as_uuid(dict_):
-    for key in dict_:
-        if key == "id" or "_id" in key:
-            dict_[key] = UUID(dict_[key])
+class _EnhanchedJSONDecoder(json.JSONDecoder):
+    def __init__(self, **kwargs):
+        super().__init__(object_hook=self.object_hook, **kwargs)
 
-    return dict_
+    def object_hook(self, dict_):
+        for key, value in dict_.items():
+            if key == "id" or "_id" in key:
+                dict_[key] = UUID(value)
+            elif isinstance(value, str):
+                if date := (
+                    self.try_convert_date_to_string(value)
+                    or self.try_convert_datetime_to_string(value)
+                ):
+                    dict_[key] = date
+
+        return dict_
+
+    def try_convert_date_to_string(self, v: str) -> dt.date | None:
+        try:
+            return dt.date.fromisoformat(v)
+        except ValueError:
+            return None
+
+    def try_convert_datetime_to_string(self, v: str) -> dt.datetime | None:
+        try:
+            return dt.datetime.fromisoformat(v)
+        except ValueError:
+            return None
+
+
+# # pass
+# def object_hook(self, dict_):
+#     for key in dict_:
+#         if key == "id" or "_id" in key:
+#             dict_[key] = UUID(dict_[key])
+
+#     return dict_
 
 
 def loads(data: str, **kwargs):
-    return json.loads(data, object_hook=_id_as_uuid, **kwargs)
+    # return json.loads(data, object_hook=_id_as_uuid, **kwargs)
+    return json.loads(data, cls=_EnhanchedJSONDecoder, **kwargs)
 
 
 def dumps(data, **kwargs):
