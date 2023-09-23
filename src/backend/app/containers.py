5ba9850ad902987c02980d4a559b56204276ca09
Joaquin Neira <joaquin.neira@houm.com>
2023-09-23 13:16:07 -0300

    add choosing of database

--- src/backend/app/containers.py
+++ src/backend/app/containers.py
@@ -1,22 +1,22 @@
+from __future__ import annotations
+
 from dependency_injector import containers, providers
 
 from app.services.authentication_service import AuthenticationService
 from app.services.io_service import IOService
-from app.services.json_database_service.todos_json_database_service import (
-    TodosJsonDatabaseService,
-)
 from app.services.json_database_service.users_json_database_service import (
     UsersJsonDatabaseService,
 )
+from app.services.sql_database_service.engine import engine
+from app.services.sql_database_service.todos_service import TodosService
+from app.services.sql_database_service.users_service import UsersService
 
 
 class Container(containers.DeclarativeContainer):
     @classmethod
-    def add_config(cls) -> type["Container"]:
+    def add_config(cls) -> type[Container]:
         cls.config = providers.Configuration()
-        cls.config.json_database.directory_path.from_env(
-            "DATABASE_PATH", "./database/"
-        )
+        cls.config.database.from_env("DATABASE")
 
         cls.wiring_config = containers.WiringConfiguration(
             packages=["app.resources"]
@@ -25,11 +25,19 @@ class Container(containers.DeclarativeContainer):
         return cls
 
     @classmethod
-    def add_json_database_services(cls) -> type["Container"]:
+    def add_json_database_services(cls) -> type[Container]:
+        cls.config.json_database.directory_path.from_env(
+            "DATABASE_PATH", "./database/"
+        )
+
         cls.users_io_service = providers.Resource(
             IOService.create_service,
             cls.config.json_database.directory_path(),
             "users.json",
+        )
+
+        cls.users_service = providers.Factory(
+            UsersJsonDatabaseService, io_service=cls.users_io_service
         )
 
         cls.todo_io_service = providers.Resource(
@@ -38,18 +46,33 @@ class Container(containers.DeclarativeContainer):
             "todos.json",
         )
 
+        cls.todos_service = providers.Factory(
+            UsersJsonDatabaseService, io_service=cls.todo_io_service
+        )
+
         return cls
 
     @classmethod
-    def add_services(cls) -> type["Container"]:
-        cls.users_service = providers.Factory(
-            UsersJsonDatabaseService, io_service=cls.users_io_service
-        )
+    def add_postgresql_services(cls) -> type[Container]:
+        cls.engine = providers.Singleton(engine)
 
-        cls.todos_service = providers.Factory(
-            TodosJsonDatabaseService, io_service=cls.todo_io_service
-        )
+        cls.users_service = providers.Factory(UsersService, engine=cls.engine)
 
+        cls.todos_service = providers.Factory(TodosService, engine=cls.engine)
+
+        return cls
+
+    @classmethod
+    def add_database_services(cls) -> type[Container]:
+        if cls.config.database() == "json":
+            cls.add_json_database_services()
+        elif cls.config.database() == "postgresql":
+            cls.add_postgresql_services()
+
+        return cls
+
+    @classmethod
+    def add_services(cls) -> type[Container]:
         cls.authentication_service = providers.Factory(
             AuthenticationService, user_service=cls.users_service
         )
@@ -57,4 +80,4 @@ class Container(containers.DeclarativeContainer):
         return cls
 
 
-Container.add_config().add_json_database_services().add_services()
+Container.add_config().add_database_services().add_services()
--- src/backend/app/services/sql_database_service/engine.py
+++ src/backend/app/services/sql_database_service/engine.py
@@ -1,7 +1,16 @@
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 
-engine = create_async_engine(
-    "postgresql+asyncpg://postgres:postgres@localhost:5432/todo_db",
-    echo=True,
-    future=True,
-)
+from app.services.sql_database_service.models import metadata_obj
+
+
+async def engine() -> AsyncEngine:
+    engine = create_async_engine(
+        "postgresql+asyncpg://postgres:postgres@postgres_db:5432/todo_db",
+        echo=True,
+        future=True,
+    )
+
+    async with engine.begin() as conn:
+        await conn.run_sync(metadata_obj.create_all)
+
+    return engine
