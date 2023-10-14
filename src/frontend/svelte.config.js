6f81c41cf3ad99a0b7bf1d4e69cc32bcd1518b0f
Joaquín Neira <github@joaquinneira.fastmail.com>
2023-10-14 00:17:28 -0300

    reinstall tailwind

--- src/frontend/svelte.config.js
+++ src/frontend/svelte.config.js
@@ -1,9 +1,11 @@
 import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
+    preprocess: vitePreprocess(),
     alias: {
       "#components": "src/components",
       "#services": "src/services",
