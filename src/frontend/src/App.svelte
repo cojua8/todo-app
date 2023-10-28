999668781b57964799942ae19e444b91f9960f29
Joaquín Neira <github@joaquinneira.fastmail.com>
2023-10-28 18:54:48 -0300

    add router

--- src/frontend/src/App.svelte
+++ src/frontend/src/App.svelte
@@ -1,49 +1,29 @@
 <script>
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "/vite.svg";
-  import Counter from "./lib/Counter.svelte";
+  import page from "page";
+
+  import Home from "./pages/Home.svelte";
+  import Login from "./pages/Login.svelte";
+  import Signup from "./pages/Signup.svelte";
+  import NavBar from "./components/NavBar.svelte";
+
+  let currentPage = Home;
+  page("/", () => (currentPage = Home));
+  page("/login", () => (currentPage = Login));
+  page("/signup", () => (currentPage = Signup));
+  page();
+
+  let year = new Date().getFullYear();
 </script>
 
 <main>
+  <NavBar />
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <div class="mt-20">
+      <svelte:component this={currentPage} />
+    </div>
+
+    <p class="text-center text-gray-500 text-xs">
+      &copy;{year} Joaquin. All rights reserved.
+    </p>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
--- src/frontend/src/components/NavBar.svelte
+++ src/frontend/src/components/NavBar.svelte
@@ -1,6 +1,4 @@
-<nav
-  class="fixed top-0 h-10 w-full p-2 bg-gray-700 text-white flex flex-auto items-center"
->
+<nav class="fixed top-0 h-10 w-full p-2 flex flex-auto items-center">
   <h2 class="ml-2 mr-4"><a href="/">My TODO App</a></h2>
   <div class="flex items-center flex-row w-40">
     <a href="/signup">Signup</a>
