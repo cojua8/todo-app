e2340cc71eb262b6634de04c35a96db722244e23
Joaquin Neira <joaquin.neira@houm.com>
2023-12-01 10:25:41 -0300

    add todo list

---
+++ src/frontend/src/lib/TodoItem.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let id;
+  export let owner_id;
+  export let description;
+  export let due_date;
+  export let completed;
+  export let date_created;
+</script>
+
+<div class="m-2 px-2 border rounded">
+  <div class="flex flex-row flex-grow justify-between align-middle">
+    <p>{description}</p>
+    <p>{due_date}</p>
+    <p>{date_created}</p>
+    <p>{completed ? "✅" : "❌"}</p>
+  </div>
+</div>
--- src/frontend/src/pages/Dashboard.svelte
+++ src/frontend/src/pages/Dashboard.svelte
@@ -1,6 +1,42 @@
 <script>
   import page from "page";
+
+  import { loggedUser } from "../stores/UserStore";
+  import { onMount } from "svelte";
+  import { getUserTodos } from "../services/TodoApi";
+  import TodoItem from "../lib/TodoItem.svelte";
+
+  let todos = [];
+  let user;
+  loggedUser.subscribe((value) => {
+    user = value;
+  });
+
+  onMount(async () => {
+    if (!user) {
+      page.redirect("/");
+      console.log("no logged user");
+    }
+
+    todos = await (await getUserTodos(user.id)).json();
+  });
+
+  const logout = () => {
+    page.redirect("/");
+    loggedUser.set(null);
+  };
 </script>
 
-dashboard
-<button on:click={() => page("/")}>logout</button>
+<div class="flex flex-col">
+  <ul class="flex flex-col items-center">
+    {#each todos as todo}
+      <li class="w-1/2">
+        <TodoItem {...todo} />
+      </li>
+    {/each}
+  </ul>
+  <br />
+  <button on:click={logout} class="px-3 py-1 border rounded w-52 self-center"
+    >Logout</button
+  >
+</div>
