e3f383cb3d33f36d949a689997448f3b8daef954
Joaquin Neira <github@joaquinneira.fastmail.com>
2023-12-05 12:45:54 -0300

    toggle completed

--- src/frontend/src/lib/TodoListItem.svelte
+++ src/frontend/src/lib/TodoListItem.svelte
@@ -1,16 +1,26 @@
 <script>
-  export let description;
-  export let dueDate;
-  export let completed;
-  export let dateCreated;
+  import { updateTodoItem } from "../services/TodoApi";
+
+  export let todo;
+
+  const toggleCompleted = async () => {
+    let updatedTodo = { ...todo, completed: !todo.completed };
+
+    await updateTodoItem(updatedTodo);
+    // how to tell dashboard to reload the todos?
+    //this is a workaround (loses coupling with real data)
+    todo.completed = !todo.completed;
+  };
 </script>
 
 <div class="m-2 px-2 h-10 border rounded flex items-center">
   <div class="flex flex-row flex-grow justify-between">
-    <p>{description}</p>
-    <p>{dueDate}</p>
-    <p>{dateCreated}</p>
-    <p>{completed ? "✅" : "❌"}</p>
+    <p>{todo.description}</p>
+    <p>{todo.dueDate}</p>
+    <p>{todo.dateCreated}</p>
+    <button on:click={() => toggleCompleted()}>
+      {todo.completed ? "✅" : "❌"}
+    </button>
   </div>
   <button on:click>edit</button>
 </div>
--- src/frontend/src/lib/TodoListing.svelte
+++ src/frontend/src/lib/TodoListing.svelte
@@ -8,7 +8,7 @@
 <ul class="w-3/5">
   {#each todos.toSorted( (a, b) => a.dateCreated.localeCompare(b.dateCreated) ) as todo}
     <li>
-      <TodoListItem on:click={() => selectedTodo.set(todo)} {...todo} />
+      <TodoListItem on:click={() => selectedTodo.set(todo)} {todo} />
     </li>
   {/each}
 </ul>
