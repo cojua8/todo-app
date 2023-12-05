ad090725d43ae37f50576d38e9df017f94e7aa29
Joaquin Neira <github@joaquinneira.fastmail.com>
2023-12-05 12:25:08 -0300

    add todo modal

---
+++ src/frontend/src/lib/TodoModal.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { selectedTodo } from "../stores/TodoStore";
+  import TodoForm from "./TodoForm.svelte";
+  import Modal from "./basics/Modal.svelte";
+</script>
+
+<Modal
+  on:click={() => selectedTodo.set(null)}
+  on:keydown={(event) => event.key === "Escape" && selectedTodo.set(null)}
+  class="w-3/5"
+>
+  <TodoForm todo={$selectedTodo} />
+</Modal>
