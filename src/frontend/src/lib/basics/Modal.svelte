a6b176cc2289eea417f2117a998f862df86ff371
Joaquin Neira <github@joaquinneira.fastmail.com>
2023-12-05 12:24:54 -0300

    add modal

---
+++ src/frontend/src/lib/basics/Modal.svelte
@@ -0,0 +1,12 @@
+<div
+  on:click
+  on:keydown
+  role="button"
+  tabindex="0"
+  class="bg-black fixed left-0 top-0 w-screen h-screen z-40 opacity-50 cursor-default"
+/>
+<div class="relative z-50 bg-white rounded-xl {$$restProps.class}">
+  <div class="p-2">
+    <slot />
+  </div>
+</div>
