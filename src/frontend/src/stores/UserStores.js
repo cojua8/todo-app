85e1d072712e4ed1877516afeb6b579cac8ff552
Joaquín Neira <github@joaquinneira.fastmail.com>
2024-05-21 13:13:33 -0400

    store user session in local storage

--- src/frontend/src/stores/UserStores.js
+++ src/frontend/src/stores/UserStores.js
@@ -1,3 +1,9 @@
 import { writable } from "svelte/store";
 
-export const loggedUser = writable(null);
+export const loggedUser = writable(
+  JSON.parse(localStorage.getItem("loggedUser")),
+);
+
+loggedUser.subscribe((value) => {
+  localStorage.setItem("loggedUser", JSON.stringify(value));
+});
