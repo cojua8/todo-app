fb44e291320314dec88d47ac46f2d4723bcdc897
Joaquin Neira <github@joaquinneira.fastmail.com>
2024-05-22 18:54:58 -0400

    rename method

--- src/backend/app/utils/json_utils.py
+++ src/backend/app/utils/json_utils.py
@@ -22,29 +22,29 @@ class _EnhancedJSONEncoder(json.JSONEncoder):
 
 class _EnhanchedJSONDecoder(json.JSONDecoder):
     def __init__(self, **kwargs):
-        super().__init__(object_hook=self.object_hook, **kwargs)
+        super().__init__(object_hook=self._object_hook, **kwargs)
 
-    def object_hook(self, dict_: dict) -> dict:
+    def _object_hook(self, dict_: dict) -> dict:
         for key, value in dict_.items():
             if key == "id" or "_id" in key:
                 dict_[key] = UUID(value)
             elif isinstance(value, str) and (
                 date := (
-                    self.try_convert_date_to_string(value)
-                    or self.try_convert_datetime_to_string(value)
+                    self._try_convert_date_to_string(value)
+                    or self._try_convert_datetime_to_string(value)
                 )
             ):
                 dict_[key] = date
 
         return dict_
 
-    def try_convert_date_to_string(self, v: str) -> dt.date | None:
+    def _try_convert_date_to_string(self, v: str) -> dt.date | None:
         try:
             return dt.date.fromisoformat(v)
         except ValueError:
             return None
 
-    def try_convert_datetime_to_string(self, v: str) -> dt.datetime | None:
+    def _try_convert_datetime_to_string(self, v: str) -> dt.datetime | None:
         try:
             return dt.datetime.fromisoformat(v)
         except ValueError:
