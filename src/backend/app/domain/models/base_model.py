f1ac66502fb58cbfbf12f956faa0067b19e0c405
Joaquín Neira <github@joaquinneira.fastmail.com>
2024-05-21 21:24:35 -0400

    remove base domain model config

--- src/backend/app/domain/models/base_model.py
+++ src/backend/app/domain/models/base_model.py
@@ -1,15 +1,7 @@
 from uuid import UUID, uuid4
 
-from pydantic import BaseModel as PydanticBaseModel, ConfigDict, Field
-
-
-def to_camel(string: str) -> str:
-    words = string.split("_")
-    capital_words = "".join(word.capitalize() for word in words[1:])
-    return words[0] + capital_words
+from pydantic import BaseModel as PydanticBaseModel, Field
 
 
 class BaseModel(PydanticBaseModel):
-    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
-
     id: UUID = Field(default_factory=uuid4)  # noqa: A003
