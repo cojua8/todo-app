6cb0a0a7aec4cd2ee5e3f49f8d0ef3fe69a0196f
Joaquín Neira <github@joaquinneira.fastmail.com>
2025-04-22 03:11:43 +0000

    action db connection

--- src/backend/tests/services/sqlalchemy_database/conftest.py
+++ src/backend/tests/services/sqlalchemy_database/conftest.py
@@ -1,20 +1,23 @@
 import pytest
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
 from app.infrastructure.sql_database_service.engine import engine
 from app.infrastructure.sql_database_service.models import metadata_obj
-from app.settings import SqlDBSettings
+
+
+class SqlDBTestSettings(BaseSettings):
+    db_dialect: str = Field("postgresql", validation_alias="DB_TEST_DIALECT")
+    db_username: str = Field("postgres", validation_alias="DB_TEST_USERNAME")
+    db_password: str = Field("postgres", validation_alias="DB_TEST_PASSWORD")
+    db_host: str = Field("postgres_db", validation_alias="DB_TEST_HOST")
+    db_port: int = Field(5432, validation_alias="DB_TEST_PORT")
+    db_name: str = Field("app_db", validation_alias="DB_TEST_NAME")
 
 
 @pytest.fixture
 def sqlalchemy_settings():
-    return SqlDBSettings.model_construct(
-        db_dialect="postgresql",
-        db_username="postgres",
-        db_password="postgres",  # noqa: S106
-        db_host="postgres_db",
-        db_port=5432,
-        db_name="app_db",
-    )
+    return SqlDBTestSettings()  # type:ignore [reportCallIssue]
 
 
 @pytest.fixture
